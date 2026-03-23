eb44761423a510970dbff91c122d7c54ab47416b
8Altair <dino.ahmetbegovic@gmail.com>
2026-03-23 20:55:45 +0100

    Created a new directory for Model. Created a new class MediaFile.cs. Added basic MediaFile class content.

---
+++ Deimos.UI/Models/MediaFile.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Media;
+
+
+namespace Deimos.UI.Models;
+
+public class MediaFile
+{
+    public string? Title { get; set; }
+    public string? FilePath { get; set; }
+    public string? ImagePath { get; set; }
+    public TimeSpan Duration { get; set; }
+    public string? Artist { get; set; }
+    public string? Album { get; set; }
+    public bool IsPlaying { get; set; }
+
+    public MediaFile()
+    {
+        
+    }
+}
