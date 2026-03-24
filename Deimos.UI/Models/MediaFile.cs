bcbdd829b05b1efc9a497b4c750d304a32c59a49
8Altair <dino.ahmetbegovic@gmail.com>
2026-03-24 14:10:50 +0100

    Converted auto-properties to full properties. Created OnPropertyChanged method for triggering UI update. Overrode a method ToString for list view items correct display.

--- Deimos.UI/Models/MediaFile.cs
+++ Deimos.UI/Models/MediaFile.cs
@@ -1,20 +1,123 @@
-﻿using System.Windows.Media;
+﻿using System.ComponentModel;
 
 
 namespace Deimos.UI.Models;
 
-public class MediaFile
+public class MediaFile: INotifyPropertyChanged
 {
-    public string? Title { get; set; }
-    public string? FilePath { get; set; }
-    public string? ImagePath { get; set; }
-    public TimeSpan Duration { get; set; }
-    public string? Artist { get; set; }
-    public string? Album { get; set; }
-    public bool IsPlaying { get; set; }
+    private string? _title;
+    private string? _filePath;
+    private string? _imagePath;
+    private TimeSpan _duration;
+    private string? _artist;
+    private string? _album;
+    private bool _isPlaying;
 
+    public string? Title
+    {
+        get => _title;
+        set
+        {
+            if (value != _title)
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+    }
+    
+    public string? FilePath
+    {
+        get => _filePath;
+        set
+        {
+            if (value != _filePath)
+            {
+                _filePath = value;
+                OnPropertyChanged(nameof(FilePath));
+            }
+        }
+    }
+    
+    public string? ImagePath
+    {
+        get => _imagePath;
+        set
+        {
+            if (value != _imagePath)
+            {
+                _imagePath = value;
+                OnPropertyChanged(nameof(ImagePath));
+            }
+        }
+    }
+    
+    public TimeSpan Duration
+    {
+        get => _duration;
+        set
+        {
+            if (value != _duration)
+            {
+                _duration = value;
+                OnPropertyChanged(nameof(Duration));
+            }
+        }
+    }
+    
+    public string? Artist
+    {
+        get => _artist;
+        set
+        {
+            if (_artist != value)
+            {
+                _artist = value;
+                OnPropertyChanged(nameof(Artist));
+            }
+        }
+    }
+    
+    public string? Album
+    {
+        get => _album;
+        set
+        {
+            if (value != _album)
+            {
+                _album = value;
+                OnPropertyChanged(nameof(Album));
+            }
+        }
+    }
+    
+    public bool IsPlaying
+    {
+        get => _isPlaying;
+        set
+        {
+            if (_isPlaying != value)
+            {
+                _isPlaying = value;
+                OnPropertyChanged(nameof(IsPlaying));
+            }
+        }
+    }
+    
     public MediaFile()
     {
         
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;  // Event required by the INotifyPropertyChanged
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));  // Notify the UI that property propertyName has changed on this object, if not null
+    }
+
+    public override string ToString()
+    {
+        return Title ?? "(Untitled)";
     }
 }
